5f34d9c63c60ccc668a7ff8d29a6a7596f36b44b
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2017-03-09 16:28:45 +0700

    Added ability to list files and nodes then insert a snippet

--- edge/extension.ts
+++ edge/extension.ts
@@ -1,28 +1,140 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
+import * as path from 'path'
+import * as fs from 'fs'
+import * as _ from 'lodash'
+import { Minimatch } from 'minimatch'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    const config = vscode.workspace.getConfiguration('haste')
+    const filePatterns = config.get('files', []) as Array<{ path: string, code: string | string[], temp: (object) => string }>
+    const nodePatterns = config.get('nodes', []) as Array<{ name: string, code: string | string[], temp: (object) => string, exec: (string) => boolean }>
+    const insertAtTheTopMost = config.get('insertAtTheTopMost') as boolean
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "haste" is now active!');
+    filePatterns.forEach(pattern => {
+        pattern.temp = _.template(_.isArray(pattern.code) ? pattern.code.join('\n') : pattern.code)
+    })
+    nodePatterns.forEach(pattern => {
+        const matcher = new Minimatch(pattern.name)
+        pattern.exec = matcher.match.bind(matcher)
+        pattern.temp = _.template(_.isArray(pattern.code) ? pattern.code.join('\n') : pattern.code)
+    })
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    const WIN_SLASH = /\\/g
+    const ROOT_PATH = new RegExp('^' + _.escapeRegExp(vscode.workspace.rootPath))
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+    let nodeModules: Array<{ name: string, vers: string, temp?: (object) => string }> = []
+    if (fs.existsSync(path.join(vscode.workspace.rootPath, 'package.json'))) {
+        const packageJson = require(path.join(vscode.workspace.rootPath, 'package.json'))
+        nodeModules = _.chain([_.keys(packageJson.devDependencies), _.keys(packageJson.dependencies)])
+            .flatten<string>()
+            .sortBy()
+            .map(nodeName => {
+                try {
+                    const packageJson = require(path.join(vscode.workspace.rootPath, 'node_modules', nodeName, 'package.json'))
+                    if (packageJson.version) {
+                        return { name: nodeName, vers: packageJson.version as string }
+                    } else {
+                        return null
+                    }
+                } catch (ex) {
+                    return null
+                }
+            })
+            .compact()
+            .map(nodeModule => {
+                const pattern = nodePatterns.find(pattern => pattern.exec(nodeModule.name))
+                if (pattern) {
+                    return { ...nodeModule, temp: pattern.temp }
+                } else {
+                    return null
+                }
+            })
+            .compact()
+            .value()
+    }
 
-    context.subscriptions.push(disposable);
+    let disposable = vscode.commands.registerCommand('extension.haste', async () => {
+        const currentDocument = vscode.window.activeTextEditor.document
+        const currentFilePath = currentDocument.fileName
+
+        const items = []
+
+        // currentDocument.getText()
+        nodeModules.forEach(nodeModule => {
+            items.push({
+                label: nodeModule.name,
+                description: nodeModule.vers,
+                type: 'node',
+                name: nodeModule.name,
+                temp: nodeModule.temp,
+            })
+        })
+
+        for (let index = 0; index < filePatterns.length; index++) {
+            const pattern = filePatterns[index]
+            const files = await vscode.workspace.findFiles(pattern.path, null, 2000)
+
+            files.forEach(file => {
+                if (file.fsPath !== currentFilePath) {
+                    items.push({
+                        label: path.basename(file.path),
+                        description: file.fsPath.replace(ROOT_PATH, '').replace(WIN_SLASH, '/'),
+                        type: 'file',
+                        path: file.fsPath,
+                        temp: pattern.temp,
+                    })
+                }
+            })
+        }
+
+        const editor = vscode.window.activeTextEditor
+        if (!editor) {
+            return null
+        }
+
+        const select = await vscode.window.showQuickPick(items, { placeHolder: 'Type a file path or node module name' })
+        if (!select) {
+            return null
+        }
+
+        let code: string
+        if (select.type === 'node') {
+            code = select.temp({
+                _, // Lodash
+                nodeName: select.name,
+            })
+
+        } else if (select.type === 'file') {
+            let selectRelativePath = path.relative(path.dirname(currentFilePath), select.path).replace(WIN_SLASH, '/')
+            if (selectRelativePath.startsWith('../') === false) {
+                selectRelativePath = './' + selectRelativePath
+            }
+
+            const extension = path.extname(select.path)
+            const selectFileNameWithoutExtension = _.camelCase(path.basename(select.path).replace(new RegExp(_.escapeRegExp(extension) + '$'), ''))
+
+            code = select.temp({
+                _, // Lodash
+                fullPath: select.path,
+                filePath: selectRelativePath,
+                fileName: selectFileNameWithoutExtension,
+                fileExtn: extension,
+            })
+        }
+
+        editor.edit(builder => {
+            builder.insert(insertAtTheTopMost ? new vscode.Position(0, 0) : editor.selection.active, code);
+        })
+    })
+
+    vscode.workspace.onDidChangeConfiguration(() => {
+        vscode.window.showInformationMessage('VS Code must be restarted in order to make changes to Haste extension.')
+    })
+
+    context.subscriptions.push(disposable)
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
